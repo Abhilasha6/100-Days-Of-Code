b1f2e34a7f787173e2405d6c8534ef0d1bdee01f
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-03-14 21:20:29 +0530

    Longest Common Prefix solution

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,31 @@
+public class LongestCommonPrefix {
+    class Solution {
+        public String longestCommonPrefix(String[] strs) {
+            String lcp="";
+            char k;
+            int n=strs.length;
+            
+            int min = strs[0].length();
+            for (int i = 1; i < n; i++)
+            {
+                if (strs[i].length() < min)
+                {
+                    min = strs[i].length();
+                }
+            }
+            
+            for(int i=0;i<min;i++)
+            {
+                k=strs[0].charAt(i);
+                for(int j=1;j<n;j++)
+                {
+                    if(k!=strs[j].charAt(i))
+                        return lcp;
+                        // lcp+=k;
+                }
+                lcp+=k;
+            }
+            return lcp;
+        }
+    }
+}
