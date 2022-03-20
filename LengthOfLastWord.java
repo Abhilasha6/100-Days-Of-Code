b651ee8691f626cd4da5b0f325610a421cc77b66
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-03-20 23:01:54 +0530

    Length of Last Word solution

--- LengthOfLastWord.java
+++ LengthOfLastWord.java
@@ -1,3 +1,19 @@
 public class LengthOfLastWord {
-    
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        // String st="";
+        int llw=0;
+        int k= s.lastIndexOf(" ");
+        for(int i=s.length()-1; i>=0;i--)
+        {
+            // st=st+s.charAt(i);
+            if(s.charAt(i)!=' ')
+                llw++;
+            else
+                break;
+        }
+        // return st.trim().length();
+        return llw;
+        
+    }
 }
