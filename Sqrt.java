c71f56ca454f56cf84baae2d66c81d5460f05c62
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-03-24 20:55:40 +0530

    solution

---
+++ Sqrt.java
@@ -0,0 +1,13 @@
+public class Sqrt() {
+    class Solution {
+        public int mySqrt(int x) {
+            double root=-1,i=0,j=x,k;
+            for(int i=1;i<n/2;i++)
+                if(i*i==n)
+                    return i;
+           
+            
+           
+        }
+    }
+}
