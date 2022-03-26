55e7b65c1dd1fc37bcc5578d10d6766c776dfbf7
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-03-26 21:41:52 +0530

    Majority Element solution

--- MajorityElement.java
+++ MajorityElement.java
@@ -1,3 +1,18 @@
+//day16
 public class MajorityElement {
-    
+    public int majorityElement(int[] nums) {
+        int cnt;
+        for(int i=0;i<nums.length;i++)
+        {
+            cnt=0;
+            for(int j=0;j<nums.length;j++){
+                if(nums[i]==nums[j]){
+                cnt++;
+            }
+            }
+           if(cnt>nums.length/2)
+               return nums[i];
+        }
+        return 0;
+    }
 }
