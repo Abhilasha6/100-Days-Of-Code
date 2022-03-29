343e5153ee8ccb660cbdbda86dedc9f86190e7cb
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-03-29 20:00:14 +0530

    Missing Number solution

--- MissingNumber.java
+++ MissingNumber.java
@@ -1,4 +1,14 @@
 //day 19
 public class MissingNumber {
-    
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==i)
+                continue;
+            else
+                return i;
+        }
+        return nums.length;
+    }
 }
