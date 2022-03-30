e4e674c63a1ad619bd199c3f270849e685c06667
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-03-30 20:46:23 +0530

    Contains Duplicate (part 2) Solution

--- ContainsDuplicateII.java
+++ ContainsDuplicateII.java
@@ -2,11 +2,22 @@ public class ContainsDuplicateII {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         // Arrays.sort(nums);
         
-        for(int i=0;i<nums.length;i++)
+        //brute force algo works effectively but it may timeout for very large input array
+        // for(int i=0;i<nums.length;i++)
+        // {
+        //     for(int j=i+1;j<nums.length;j++)            
+        //         if((nums[i]==nums[j] )&& (Math.abs(i-j)<=k))
+        //             return true;
+        // }
+        // return false;
+
+        // for more efficiency we can use HashMap or HashSet algo
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i=0; i<nums.length; i++)
         {
-            for(int j=i+1;j<nums.length;j++)            
-                if((nums[i]==nums[j] )&& (Math.abs(i-j)<=k))
-                    return true;
+            if(map.containsKey(nums[i]) && i - map.get(nums[i]) <= k)
+                return true;    
+            map.put(nums[i] , i);
         }
         return false;
     }
