c0275b6e05d91eda473a4a7e8ecc5740ea0670bd
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-03-31 20:00:54 +0530

    Move Zeroes final solution

--- MoveZeroes.java
+++ MoveZeroes.java
@@ -1,24 +1,26 @@
 //day 21
 public class MoveZeroes {
         public void moveZeroes(int[] nums) {
-            int cnt=0,i,temp,j=0;
+            int cnt=0,i,j=0;
             for(i=0;i<nums.length;i++)
             {
                 if(nums[i]!=0)
                 {
-                    temp=nums[i];
-                    for( j=0;j<nums.length;i++)
-                        if(nums[j]!=0){
-                            nums[i]=nums[j];
-                            nums[j]=temp;
-                            break;
-                        }
-                    
+                    // temp=nums[i];
+                    // for(int j=0;j<nums.length;i++)
+                    //     if(nums[j]!=0){
+                    //         nums[i]=nums[j];
+                    //         nums[j]=temp;
+                    //         break;
+                    //     }
+                    nums[j]= nums[i]; 
+                    j++;
                 }
                 else
-                    continue;
+                    cnt++;
             }
+            for(i=0;i<cnt;i++)
+                nums[j++]=0;
             // return nums;
         }
-    
 }
