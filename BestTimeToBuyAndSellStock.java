c24e9f6631e4f37a171f82b6295d6debea33615c
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-07 20:24:15 +0530

    brute force ans & optimised solution also included

--- BestTimeToBuyAndSellStock.java
+++ BestTimeToBuyAndSellStock.java
@@ -1,10 +1,11 @@
 public class BestTimeToBuyAndSellStock {
+
+//Brute force approach- works perfect but time limit may exceed for very large data
     public int maxProfit(int[] prices) {
         int buy = 0, sell = 0, mp = 0;
         if (prices.length == 1)
             return 0;
         if (prices.length == 2) {
-            // return Math.max(prices[0],prices[1])-Math.min(prices[0],prices[1]);
             if (prices[0] >= prices[1])
                 return 0;
             else
@@ -12,11 +13,6 @@ public class BestTimeToBuyAndSellStock {
         }
         for (int i = 0; i < prices.length; i++) {
             for (int j = i + 1; j < prices.length; j++) {
-
-                // if(prices[i]>max)
-                // max=prices[i];
-                // else if(prices[i]<min)
-                // min=prices[i];
                 if ((prices[j] - prices[i]) > mp) {
                     buy = prices[i];
                     sell = prices[j];
@@ -24,8 +20,19 @@ public class BestTimeToBuyAndSellStock {
                 }
             }
         }
-        // p=max-min;
         return mp;
     }
+
+
+//optimised solution that works for huge input data as well
+public int maxProfit1(int[] prices) {
+    int profit = 0;
+    int buy = 0; // index when buying
+    for(int i=0;i<prices.length;i++){
+        if(prices[buy] > prices[i]) buy = i;
+        profit = Math.max(profit, prices[i] - prices[buy]);
+    }
+    return profit;
+}
 
 }
