c1dd197e1c91629930f9ac15b9335179d52083cd
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-09 20:44:55 +0530

    Happy Number solution using recursion

--- HappyNumber.java
+++ HappyNumber.java
@@ -1,30 +1,25 @@
+//day 29
+
 public class HappyNumber {
-    class Solution {
-        public boolean isHappy(int n) {
-            if(n==1||n==7)
-                return true;
-            else if(n<10 && n>1)
-                return false;
-            else
-             return sod(n);
+    public boolean isHappy(int n) {
+
+        return sod(n);
+
+    }
+
+    // finding digits & sum of squares of digits
+    public static boolean sod(int x) {
+        if (x == 1 || x == 7)
+            return true;
+        else if (x < 10 && x > 1)
             return false;
+        int d, ss = 0;
+        for (int i = 0; x > 0; i++) {
+            d = x % 10;
+            ss = ss + d * d;
+            x = x / 10;
         }
-        
-        //finding digits & sum of squares of digits
-        public static boolean sod(int x){
-           if (x==1 || x==7)
-                return true;
-            else if(x<10 && x>1)
-                return false;
-            int d,ss=0;
-            for(int i=0;x>0;i++)
-            {
-                d=x%10;
-                ss=ss+d*d;
-                x=x/10;
-            }
-            x=ss;
-            return sod(x);
-        } 
+        x = ss;
+        return sod(x);
     }
 }
