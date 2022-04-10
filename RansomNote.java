e05e92cba4701e8473a33e6b5390436aeb57904a
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-10 20:18:01 +0530

    Ransom Number Solution(day 30)

--- RansomNote.java
+++ RansomNote.java
@@ -1,23 +1,20 @@
+//day 30
+
 public class RansomNote {
     public boolean canConstruct(String ransomNote, String magazine) {
         //this would be useful if we were to find a particular substring/set of concatinated letters/words
         // boolean ans= magazine.contains(ransomNote);
         // return ans;
 
-        //traversing through both strings
-        String s = "";
-        for (int i = 0; i < ransomNote.length(); i++) {
-            for (int j = 0; j < magazine.length(); j++) {
-                char ch = magazine.charAt(j);
-                if (ransomNote.charAt(i) == magazine.charAt(j)) {
-                    s += magazine.charAt(j);
-                    magazine = magazine.replaceFirst(Character.toString(ransomNote.charAt(i)), "");
-                    break;
-                }
+        String s="";
+        for(int i = 0; i < ransomNote.length(); i++){
+            if(magazine.contains(Character.toString(ransomNote.charAt(i)))){
+              s+=ransomNote.charAt(i);  
+              magazine = magazine.replaceFirst(Character.toString(ransomNote.charAt(i)),"");
             }
+            if(s.equals(ransomNote))
+                return true;
         }
-        if (s == ransomNote)
-            return true;
         return false;
 
     }
