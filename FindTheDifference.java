9954d4ed1cabc6e840665ffb22c65887d89c3e87
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-12 19:43:23 +0530

    solution

---
+++ FindTheDifference.java
@@ -0,0 +1,19 @@
+public class FindTheDifference {
+        public char findTheDifference(String s, String t) {
+            if(s==null)
+                return t.charAt(0);
+            for(int i=0;i<t.length();i++)
+            {
+                if(s.contains(Character.toString(t.charAt(i))))
+                {
+                    s = s.replaceFirst(Character.toString(t.charAt(i)),"");
+                    continue;
+                }
+                else
+                {
+                    return t.charAt(i);
+                }
+            }
+        return '0';
+        }
+}
