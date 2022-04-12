2d31b8a324e5c685d9d5f057ae62c954b2dba591
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-12 19:47:48 +0530

    Finf the Difference solution

--- FindTheDifference.java
+++ FindTheDifference.java
@@ -1,21 +1,17 @@
 //day 32
 
 public class FindTheDifference {
-        public char findTheDifference(String s, String t) {
-            // if(s==null)
-            //     return t.charAt(0);
-            for(int i=0;i<t.length();i++)
-            {
-                if(s.contains(Character.toString(t.charAt(i))))
-                {
-                    s = s.replaceFirst(Character.toString(t.charAt(i)),"");
-                    continue;
-                }
-                else
-                {
-                    return t.charAt(i);
-                }
+    public char findTheDifference(String s, String t) {
+        // if(s==null)
+        // return t.charAt(0);
+        for (int i = 0; i < t.length(); i++) {
+            if (s.contains(Character.toString(t.charAt(i)))) {
+                s = s.replaceFirst(Character.toString(t.charAt(i)), "");
+                continue;
+            } else {
+                return t.charAt(i);
             }
-        return '0';
         }
+        return '0';
+    }
 }
