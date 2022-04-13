f8dbf64f34bc6777e263cbb3b769c9363d39b0aa
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-13 21:23:28 +0530

    Is Subsequence solution

--- IsSubsequence.java
+++ IsSubsequence.java
@@ -1,15 +1,26 @@
+//day 33
+
 public class IsSubsequence {
     public boolean isSubsequence(String s, String t) {
-        String subs = "";
-        // int i=0;
-        for (int i = 0; i < s.length(); i++) {
-            if (t.contains(Character.toString(s.charAt(i)))) {
-                subs += Character.toString(s.charAt(i));
-                t = t.replaceFirst(Character.toString(s.charAt(i)), "");
+        // String subs = "";
+        int i = 0;
+        // for(int i=0;i<s.length();i++)
+        // {
+        // if(t.contains(Character.toString(s.charAt(i))))
+        // {
+        // subs+=Character.toString(s.charAt(i));
+        // t=t.replaceFirst(Character.toString(s.charAt(i)),"");
+        // }
+        // }
+        // if(subs==s)
+        // return true;
+        // return false;
+
+        for (int j = 0; j < t.length() && i < s.length(); j++) {
+            if (s.charAt(i) == t.charAt(j)) {
+                i++;
             }
         }
-        if (subs == s)
-            return true;
-        return false;
+        return i == s.length();
     }
 }
