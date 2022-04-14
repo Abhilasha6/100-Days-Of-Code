34d76f434c8f1429be0fbe5288438a7f17dafc7e
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-14 21:23:26 +0530

    day 34

---
+++ LongestPalindrome.java
@@ -0,0 +1,25 @@
+public class LongestPalindrome {
+    public int longestPalindrome(String s) {
+    
+        char[] c = s.toCharArray();
+        int j = 0;
+        Arrays.sort(c);
+        
+        if(c.length == 1)
+            return 1;
+        
+        for(int i = 0; i < c.length - 1; i++)
+        {
+            if(c[i] == c[i+1])
+            {
+                j += 2;
+                i += 1;
+            }
+        }
+        
+        if(j < c.length)
+            j += 1;
+        
+        return j;
+    }
+}
