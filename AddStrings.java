d5c255c938f662eaef185257aa4a8899a19fcb82
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-15 21:20:54 +0530

    day 35

---
+++ AddStrings.java
@@ -0,0 +1,9 @@
+class AddStrings {
+    public String addStrings(String num1, String num2) {
+        int a=Integer.parseInt(num1);
+        int b=Integer.parseInt(num2);
+        String s= Integer.toString(a+b);
+        return s;
+        
+    }
+}
