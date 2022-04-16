64659eec836bc44daf2ba005374186e51029f232
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-16 21:50:50 +0530

    Number Of Segments In A String

--- NumberOfSegmentsInString.java
+++ NumberOfSegmentsInString.java
@@ -3,21 +3,24 @@
 public class NumberOfSegmentsInString {
     class Solution {
         public int countSegments(String s) {
-            int c=1;
-            s=s.trim();
-            if(s.length()==0)
+            int c = 1;
+            s = s.trim();
+            if (s.length() == 0)
                 return 0;
-            else{
-                //this method will work for all words when there are no special sequences/ escape sequences                
-                 for(int i=0;i<s.length();i++)
-            {
-                if((s.charAt(i))==' ')
-                    c++;
-                else
-                    continue;
+            else {
+                // this method will work for all words when there are no special sequences/escape sequences
+                // for (int i = 0; i < s.length(); i++) {
+                //     if ((s.charAt(i)) == ' ')
+                //         c++;
+                //     else
+                //         continue;
+                // }
+                // return c;
+
+                //for a more generalised answer we go for this solution
+                return s.trim().split("\\s+").length;
+
             }
-            return c;
-            }      
         }
     }
 }
