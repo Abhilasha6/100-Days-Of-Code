c1a6239f45c06350329ae74d20ec2246481ce27a
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-19 21:38:17 +0530

    day 38

---
+++ DetectCapital.java
@@ -0,0 +1,33 @@
+public class DetectCapital {
+    class Solution {
+        public boolean detectCapitalUse(String word) {
+            char arr[]= word.toCharArray();
+            int c=0;
+            int x=0;
+            
+          
+            
+            if(arr[0]>=65 && arr[0]<=90){
+                 for(int i=1;i<arr.length;i++){
+                    if((arr[i]>=65 && arr[i]<=90))
+                        return false;
+              }
+                x=1;
+            }       
+             if(x==1)
+                return true;
+            
+                for(int i=0;i<arr.length;i++)
+                {
+                if(arr[i]>=65 && arr[i]<=90)
+                    c++;
+                
+                }
+            
+            
+            if(c==0 || c==arr.length || x==1)
+                return true;
+            return false;
+        }
+    }
+}
