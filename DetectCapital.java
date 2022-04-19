c047f6aa567e03299a884044bb6609b459ec8b78
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-19 22:19:06 +0530

    Detect Capital updated solution

--- DetectCapital.java
+++ DetectCapital.java
@@ -1,33 +1,31 @@
+//day 38
+
 public class DetectCapital {
     class Solution {
-        public boolean detectCapitalUse(String word) {
-            char arr[]= word.toCharArray();
-            int c=0;
-            int x=0;
-            
-          
-            
-            if(arr[0]>=65 && arr[0]<=90){
-                 for(int i=1;i<arr.length;i++){
-                    if((arr[i]>=65 && arr[i]<=90))
-                        return false;
-              }
-                x=1;
-            }       
-             if(x==1)
-                return true;
-            
-                for(int i=0;i<arr.length;i++)
-                {
-                if(arr[i]>=65 && arr[i]<=90)
-                    c++;
-                
-                }
-            
-            
-            if(c==0 || c==arr.length || x==1)
-                return true;
-            return false;
-        }
+    public boolean detectCapitalUse(String word) {
+        char arr[]= word.toCharArray();
+        int c=0;
+        int x=0;
+        
+        for(int i=0;i<arr.length;i++)
+            {
+            if(arr[i]>=65 && arr[i]<=90)
+                c++;
+            }
+        if(c==0 || c==arr.length)
+            return true;
+        
+        if(arr[0]>=65 && arr[0]<=90){
+             for(int i=1;i<arr.length;i++){
+                if((arr[i]>=65 && arr[i]<=90))
+                    return false;
+          }
+            x=1;
+        }       
+         if(x==1)
+            return true;
+       
+        return false;
     }
+}
 }
