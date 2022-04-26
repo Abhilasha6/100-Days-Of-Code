b34e4f3c53dfc5888117d9060da1d20c559be26e
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-26 21:49:26 +0530

    day 44 solution

---
+++ ReverseWordsInString.java
@@ -0,0 +1,19 @@
+//day 44
+
+public class ReverseWordsInString {
+    public String reverseWords(String s) {
+        //Strings in java are immutable, so I have to work with StringBuilder
+        String[] words = s.split(" ");
+        
+        StringBuilder reversed = new StringBuilder();
+        for(String word : words){
+            for(int i=word.length()-1;i>=0;i--){
+                reversed.append(word.charAt(i));
+            }
+            reversed.append(" ");
+        }
+        //delete the last space
+        reversed.deleteCharAt(reversed.length()-1);
+        return reversed.toString();
+    }
+}
