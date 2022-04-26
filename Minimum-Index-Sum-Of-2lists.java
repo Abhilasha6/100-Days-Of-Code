ab7ae68481ce68cffed5e6d463636fa705ee8566
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-26 21:45:52 +0530

    final solution

--- Minimum-Index-Sum-Of-2lists.java
+++ Minimum-Index-Sum-Of-2lists.java
@@ -1,5 +1,9 @@
-public class Minimum-Index-Sum-Of-2lists {
-        public String[] findRestaurant(String[] list1, String[] list2) {
+//day 45
+
+public class Minimum-Index-Sum-Of-2l ists
+{
+
+    public String[] findRestaurant(String[] list1, String[] list2) {
             // char arr1[]=list1.toCharArray();
             // char arr2[]=list2.toCharArray();
             // int sum=list1.length();
@@ -10,25 +14,25 @@ public class Minimum-Index-Sum-Of-2lists {
                 for(int j=0;j<=i;j++)
                 {
                    
-                    if(arr1[i]==arr2[j])
-                    {
-                        if(sum>i+j)
-                        {
-                            sum=i+j;
-                            ans[k++]=arr1[i];
-                        }
-                    }
+                    // if(arr1[i]==arr2[j])
+                    // {
+                    //     if(sum>i+j)
+                    //     {
+                    //         sum=i+j;
+                    //         ans[k++]=arr1[i];
+                    //     }
+                    // }
                     
-                  
+                    if (j < list1.length && i-j < list2.length && list1[j].equals(list2[i-j]))
+                        ans[k++]=list1[j];
                 }
-                
+                if (k > 0)
+                    break;
             }
             String res[]=new String[k];
             for(int i=0;i<k;i++)
                 res[i]=ans[i];
             return res;
-            // }
             
         }
-
 }
