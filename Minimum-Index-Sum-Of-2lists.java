fad1731043842775574d12fb8e29de6de6ddc96a
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-26 21:44:29 +0530

    day 45

---
+++ Minimum-Index-Sum-Of-2lists.java
@@ -0,0 +1,34 @@
+public class Minimum-Index-Sum-Of-2lists {
+        public String[] findRestaurant(String[] list1, String[] list2) {
+            // char arr1[]=list1.toCharArray();
+            // char arr2[]=list2.toCharArray();
+            // int sum=list1.length();
+            int k=0;
+            String ans[]=new String[list1.length];
+            for(int i=0;i<list1.length+list2.length-1;i++)
+            {
+                for(int j=0;j<=i;j++)
+                {
+                   
+                    if(arr1[i]==arr2[j])
+                    {
+                        if(sum>i+j)
+                        {
+                            sum=i+j;
+                            ans[k++]=arr1[i];
+                        }
+                    }
+                    
+                  
+                }
+                
+            }
+            String res[]=new String[k];
+            for(int i=0;i<k;i++)
+                res[i]=ans[i];
+            return res;
+            // }
+            
+        }
+
+}
