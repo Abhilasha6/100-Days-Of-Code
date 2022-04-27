8a54c320db8e99c0665090a87ff26fc6c34c71e6
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-04-27 21:53:45 +0530

    one line solution for ToLowerCase

--- ToLowerCse.java
+++ ToLowerCse.java
@@ -1,15 +1,17 @@
+//day 46
+
 public class ToLowerCse {
     class Solution {
         public String toLowerCase(String s) {
-            String ans="";
-            for(int i=0;i<s.length();i++)
-                if(s.charAt(i)>'A' && s.charAt(i)<'Z')
-                {
-                    ans+=Character.toString((char)s.charAt(i)+32);
-                }
-            return ans;
+            // String ans="";
+            // for(int i=0;i<s.length();i++)
+            //     if(s.charAt(i)>'A' && s.charAt(i)<'Z')
+            //     {
+            //         ans+=Character.toString((char)s.charAt(i)+32);
+            //     }
+            // return ans;
+            return s.toLowerCase();
 
-            
         }
     }
 }
