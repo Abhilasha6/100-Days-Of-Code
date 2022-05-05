e0d18c8f74432e3dfa40c9c50a8d58ca3531fb63
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-05-05 21:28:34 +0530

    Implement Stack Using Queue

---
+++ ImplementStackUsingQueues.java
@@ -0,0 +1,28 @@
+//day 54
+
+public class ImplementStackUsingQueues {
+        private Queue<Integer> queue = new LinkedList<>();
+    
+        public void push(int x) {
+            queue.add(x);
+            for (int i=1; i<queue.size(); i++)
+                queue.add(queue.remove());
+        }
+    
+        public int pop() {
+            return queue.remove();
+        }
+    
+        public int top() {
+            return queue.peek();
+        }
+    
+       public boolean empty() 
+        {
+            // return queue.isEmpty();
+           if(queue.isEmpty())
+               return true;
+           else
+               return false;
+        }
+}
