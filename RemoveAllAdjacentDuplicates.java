ffa44afdb475f1b8903f7893f0282b9873ab806c
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-05-08 22:42:54 +0530

    Remove All Adjacent Duplicates solution

--- RemoveAllAdjacentDuplicates.java
+++ RemoveAllAdjacentDuplicates.java
@@ -1,19 +1,31 @@
 //day 57
 
 public class RemoveAllAdjacentDuplicates {
-        public String removeDuplicates(String s) {
-            char arr[]=new char[s.length()];
-            // Arrays.sort(arr);
-            String str=Character.toString(s.charAt(0));
-            for(int i=0;i<arr.length;i++)
-            {
-                if(i<arr.length-1 && arr[i]==arr[i+1])
-                {
-                    arr[i+1]=3'0';
+        // public String removeDuplicates(String s) {
+        //     char arr[]=new char[s.length()];
+        //     // Arrays.sort(arr);
+        //     String str=Character.toString(s.charAt(0));
+        //     for(int i=0;i<arr.length;i++)
+        //     {
+        //         if(i<arr.length-1 && arr[i]==arr[i+1])
+        //         {
+        //             arr[i+1]=3'0';
+        //         }
+        //         else
+        //             str+=arr[i];
+        //     }
+        //     return str;
+        public String removeDuplicates(String S) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : S.toCharArray()) {
+                int size = sb.length();
+                if (size > 0 && sb.charAt(size - 1) == c) { 
+                    sb.deleteCharAt(size - 1); 
                 }
-                else
-                    str+=arr[i];
+                else { 
+                    sb.append(c); 
+                }
             }
-            return str;
-      
+            return sb.toString();
+        }
     }
