6b7ae2ce9358421dfad80cee0c4c100e8d6bdde8
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-05-22 23:39:26 +0530

    2 approaches

--- MaxProductDiffBetween2Pairs.java
+++ MaxProductDiffBetween2Pairs.java
@@ -1,8 +1,34 @@
 //day 71
 
 public class MaxProductDiffBetween2Pairs {
-        public int maxProductDifference(int[] nums) {
-            Arrays.sort(nums);
-            return (nums[nums.length-1]*nums[nums.length-2] - nums[0]*nums[1]);
+    public int maxProductDifference(int[] nums) {
+
+        // Approach 1
+        Arrays.sort(nums);
+        return (nums[nums.length - 1] * nums[nums.length - 2] - nums[0] * nums[1]);
+
+        // Approach 2
+        int max1 = Integer.MIN_VALUE;
+        int max2 = Integer.MIN_VALUE;
+
+        int min1 = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+
+        for (int val : nums) {
+            if (val > max1) {
+                max2 = max1;
+                max1 = val;
+            } else if (val > max2) {
+                max2 = val;
+            }
+
+            if (val < min1) {
+                min2 = min1;
+                min1 = val;
+            } else if (val < min2) {
+                min2 = val;
+            }
         }
+        return (max1 * max2) - (min1 * min2);
+    }
 }
