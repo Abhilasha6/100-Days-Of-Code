40f8d4a357011e9422c7843f6c12386b1fc61a60
Abhilasha Panda <81556030+Abhilasha6@users.noreply.github.com>
2022-06-10 00:00:47 +0530

    day 88

--- TwoSum2.java
+++ TwoSum2.java
@@ -1,10 +1,12 @@
 //day 88
 
 public class TwoSum2 {
-    int l = 0, r = numbers.length - 1;
+    public int[] twoSum(int[] numbers, int target) {
+        int l = 0, r = numbers.length - 1;
     while (numbers[l] + numbers[r] != target) {
         if (numbers[l] + numbers[r] > target) r--;
         else l++;
     }
     return new int[]{l + 1, r + 1};
+    };
 }
